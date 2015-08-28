0fa91d17852627faa7f429028b8d88d86b178028
Topi Talvitie <topi.talvitie@iki.fi>
2015-08-28 20:16:39 +0300

    Added forgotten files.

---
+++ bmp.c
@@ -0,0 +1,204 @@
+#include "bmp.h"
+
+#include <string.h>
+
+bmp_canvas bmp_start(size_t width, size_t height) {
+	bmp_canvas canvas;
+	canvas.width = width;
+	canvas.height = height;
+	if(width > SIZE_MAX / 3) fail("bmp_start: Image too large.");
+	
+	canvas.pitch = 3 * canvas.width;
+	while(canvas.pitch % 4) {
+		++canvas.pitch;
+		if(!canvas.pitch) fail("bmp_start: Image too large.");
+	}
+	
+	if(canvas.pitch != 0 && height > SIZE_MAX / canvas.pitch) {
+		fail("bmp_start: Image too large.");
+	}
+	
+	canvas.size = canvas.pitch * height;
+	canvas.data = checked_malloc(canvas.size);
+	memset(canvas.data, 0, canvas.size);
+	
+	return canvas;
+}
+
+void bmp_write(bmp_canvas canvas, const char* filename) {
+	FILE* fp = fopen(filename, "wb");
+	if(!fp) fail("Failed to open output BMP file '%s' for writing.", filename);
+	
+	// Write header.
+	
+	// ID
+	fputc(0x42, fp);
+	fputc(0x4D, fp);
+	
+	// Size
+	uint32_t tmp_u32 = canvas.size;
+	if((size_t)tmp_u32 != canvas.size) fail("bmp_write: Image too large.");
+	tmp_u32 += 54;
+	if(tmp_u32 < 54) fail("bmp_write: Image too large.");
+	for(int i = 0; i < 4; ++i) {
+		fputc(tmp_u32 & (uint32_t)0xFF, fp);
+		tmp_u32 >>= 8;
+	}
+	
+	// Unused
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	
+	// Pixel data offset (54 bytes = header size).
+	fputc(54, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	
+	// DIB header size (40 bytes)
+	fputc(40, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	
+	// Width
+	tmp_u32 = canvas.width;
+	for(int i = 0; i < 4; ++i) {
+		fputc(tmp_u32 & (uint32_t)0xFF, fp);
+		tmp_u32 >>= 8;
+	}
+	
+	// Height
+	tmp_u32 = canvas.height;
+	for(int i = 0; i < 4; ++i) {
+		fputc(tmp_u32 & (uint32_t)0xFF, fp);
+		tmp_u32 >>= 8;
+	}
+	
+	// Color plane count (1).
+	fputc(1, fp);
+	fputc(0, fp);
+	
+	// Bits per pixel (24).
+	fputc(24, fp);
+	fputc(0, fp);
+	
+	// No compression
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	
+	// Size of the bitmap data.
+	tmp_u32 = canvas.size;
+	for(int i = 0; i < 4; ++i) {
+		fputc(tmp_u32 & (uint32_t)0xFF, fp);
+		tmp_u32 >>= 8;
+	}
+	
+	// 72 DPI
+	fputc(0x13, fp);
+	fputc(0x0B, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0x13, fp);
+	fputc(0x0B, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	
+	// Palette size (0)
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	
+	// Number of important colors in palette (0)
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	fputc(0, fp);
+	
+	// Image data
+	fwrite(canvas.data, 1, canvas.size, fp);
+	
+	fclose(fp);
+	
+	free(canvas.data);
+}
+
+size_t clamp_to_size_t(float x, size_t limit) {
+	if(x < 0.0) return 0;
+	if(x > (float)limit) return limit;
+	size_t ret = (size_t)x;
+	if(ret > limit) ret = limit;
+	return ret;
+}
+
+void bmp_draw(
+	bmp_canvas canvas,
+	float x1, float y1, float x2, float y2, float x3, float y3,
+	uint8_t r, uint8_t g, uint8_t b
+) {
+	typedef struct {
+		float x;
+		float y;
+	} vec2;
+	
+	vec2 v[3];
+	v[0].x = x1;
+	v[0].y = y1;
+	v[1].x = x2;
+	v[1].y = y2;
+	v[2].x = x3;
+	v[2].y = y3;
+	
+	// Sort by y using insertion sort.
+	for(int i = 1; i < 3; ++i) {
+		for(int j = i - 1; j >= 0; --j) {
+			if(v[j].y > v[j + 1].y) {
+				vec2 tmp = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = tmp;
+			}
+		}
+	}
+	
+	size_t y_lo = clamp_to_size_t(v[0].y, canvas.height);
+	size_t y_hi = clamp_to_size_t(v[2].y + 1.0, canvas.height);
+	
+	for(size_t y = y_lo; y < y_hi; ++y) {
+		float yd = (float)y;
+		if(!(yd >= v[0].y && yd < v[2].y)) continue;
+		
+		float t = (y - v[0].y) / (v[2].y - v[0].y);
+		float x1 = v[0].x + t * (v[2].x - v[0].x);
+		
+		float x2;
+		if(yd < v[1].y) {
+			t = (y - v[0].y) / (v[1].y - v[0].y);
+			x2 = v[0].x + t * (v[1].x - v[0].x);
+		} else {
+			t = (y - v[1].y) / (v[2].y - v[1].y);
+			x2 = v[1].x + t * (v[2].x - v[1].x);
+		}
+		if(x1 > x2) {
+			float tmp = x1;
+			x1 = x2;
+			x2 = tmp;
+		}
+		
+		size_t x_lo = clamp_to_size_t(x1, canvas.width);
+		size_t x_hi = clamp_to_size_t(x2 + 1.0, canvas.width);
+		
+		size_t pos = y * canvas.pitch + 3 * x_lo;
+		for(size_t x = x_lo; x < x_hi; ++x, pos += 3) {
+			float xd = (float)x;
+			if(!(xd >= x1 && xd < x2)) continue;
+			canvas.data[pos + 2] = r;
+			canvas.data[pos + 1] = g;
+			canvas.data[pos + 0] = b;
+		}
+	}
+}
---
+++ bmp.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "common.h"
+#include "stdint.h"
+
+/// A context for drawing a set of triangles and creating a BMP image out of
+/// them. Create with bmp_start(), draw triangles with bmp_draw() and finalize
+/// with bmp_write(). Do not access directly.
+typedef struct {
+	size_t width;
+	size_t height;
+	size_t pitch;
+	size_t size;
+	uint8_t* data;
+} bmp_canvas;
+
+/// Create BMP canvasing context.
+/// \param width Width of the image.
+/// \param height Height of the image.
+bmp_canvas bmp_start(size_t width, size_t height);
+
+/// Write a BMP containing everything drawn to a context. Destroys the context.
+/// \param canvas The context to write and free.
+/// \param filename The name of the file to write to.
+void bmp_write(bmp_canvas canvas, const char* filename);
+
+/// Draw triangle (\p x1, \p y1), (\p x2, \p y2), (\p x3, \p y3) to \p canvas
+/// with colors (\p r, \p g, \p b).
+void bmp_draw(
+	bmp_canvas canvas,
+	float x1, float y1, float x2, float y2, float x3, float y3,
+	uint8_t r, uint8_t g, uint8_t b
+);
