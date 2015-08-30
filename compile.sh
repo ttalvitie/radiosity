6ae74b9edd761e31a28e67f9260280af21eca4b6
Topi Talvitie <topi.talvitie@iki.fi>
2015-08-30 21:59:10 +0300

    Changed the default number of workers again to better use 8-core hyperthreaded computers.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc common.c draw_bmp.c main.c matrix.c radiosity.c triangle.c bmp.c raycast.c -o radiosity -g -Wall -std=gnu99 -lm -Ofast -funroll-loops -march=native -mfpmath=sse -pthread -DWORKERTHREADS=8
+gcc common.c draw_bmp.c main.c matrix.c radiosity.c triangle.c bmp.c raycast.c -o radiosity -g -Wall -std=gnu99 -lm -Ofast -funroll-loops -march=native -mfpmath=sse -pthread -DWORKERTHREADS=16
 # For matrices that do not fit in memory, add -DLARGE_MATRIX_TMPFILE_TEMPLATE=\"/path/to/tmpXXXXXX\"
